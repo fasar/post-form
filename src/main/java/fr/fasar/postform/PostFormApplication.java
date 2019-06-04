d81a7767ce355a73cd7a04879f35673cd2eff46c
SARTOR Fabien <fabien.sartor@secheron.com>
2019-06-04 20:40:15 +0200

    Add the form example from spring.

---
+++ src/main/java/fr/fasar/postform/PostFormApplication.java
@@ -0,0 +1,13 @@
+package fr.fasar.postform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostFormApplication.class, args);
+	}
+
+}
